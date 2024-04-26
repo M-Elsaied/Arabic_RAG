cc409c4f52bac5f3fedcba76356c2b57dfc5fa73
M-Elsaied <melsaied@smu.edu>
2024-04-26 09:26:12 +0300

    add synthesized model

--- app.py
+++ app.py
@@ -43,8 +43,10 @@ if uploaded_files:
     if not st.session_state['db']:
         # create_vector_db_for_files(files_info)
         embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-        db = load_local_vectordb_using_qdrant("testing_arabic", embeddings)
-        st.session_state['db'] = db
+        db_files = load_local_vectordb_using_qdrant("testing_arabic", embeddings)
+        db_tables = load_local_vectordb_using_qdrant("tables_arabic", embeddings)
+        st.session_state['db'] = db_files
+        st.session_state['db_tables'] = db_tables
         st.success('Database updated with uploaded files!')
 
 # User input for queries
@@ -53,10 +55,40 @@ user_prompt = st.text_input('Enter Your Query here..........')
 # Button to trigger the response generation
 if st.button("Press Enter"):
     if st.session_state['db']:
-        st.session_state['response'] = arabic_qa(user_prompt, st.session_state['db'])
+        st.info("""Processing your query, please be patient. This might take some time.
+
+                إن الله مع الصابرين""")
+
+        # st.session_state['response'] = arabic_qa(user_prompt, st.session_state['db'])
+        st.session_state['response'] = synthesize_responses(user_prompt, st.session_state['db'], st.session_state['db_tables'])
     else:
         st.error("No database found. Please upload a file first.")
 
 # Display the response
 if st.session_state['response']:
     message(st.session_state['response'])
+
+
+# Button to trigger the response generation
+# if st.button("Press Enter"):
+#     if 'db' in st.session_state and 'db_tables' in st.session_state:
+#         # Get response from a hard ensemble model
+#         hybrid_response = arabic_qa_files_and_tables(user_prompt, st.session_state['db'], st.session_state['db_tables'])
+
+#         # Get response from a hybrid model using both databases
+#         hard_ensemble_response= synthesize_responses(user_prompt, st.session_state['db'], st.session_state['db_tables'])
+
+#         # Save responses to session state
+#         st.session_state['hard_ensemble_response'] = hard_ensemble_response
+#         st.session_state['hybrid_response'] = hybrid_response
+#     else:
+#         st.error("No database found. Please upload a file first.")
+
+# # Display the responses
+# if 'hard_ensemble_response' in st.session_state:
+#     st.subheader("Response from Hard Ensemble Model:")
+#     st.write(st.session_state['hard_ensemble_response'])
+
+# if 'hybrid_response' in st.session_state:
+#     st.subheader("Response from Hybrid Model:")
+#     st.write(st.session_state['hybrid_response'])
